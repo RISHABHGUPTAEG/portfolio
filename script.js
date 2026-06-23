168860291c669d73d42b1e1841e4adcfc6146fec
Rishu Kankane <grkgupta70@gmail.com>
2026-06-23 12:05:49 +0530

    Added profile image and resume

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-window.addEventListener("load", () => {
-    document.body.style.opacity = "1";
-});
+const texts = [
+    "AI Developer",
+    "Flutter Developer",
+    "Software Engineer",
+    "Web Developer",
+    "Machine Learning Enthusiast"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+
+    currentText = texts[count];
+
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+
+        count++;
+        index = 0;
+
+        setTimeout(type, 1200);
+    }
+    else{
+        setTimeout(type, 100);
+    }
+
+})();
